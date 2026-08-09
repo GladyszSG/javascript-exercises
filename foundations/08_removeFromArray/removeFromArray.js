a44917f337abfe136f5c6b8c4e58a03a6fac1724
GladyszSG <189050190+GladyszSG@users.noreply.github.com>
2026-08-09 22:58:44 +0200

    Finish exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+function removeFromArray(arr, ...argument) {
+    for (let i = 0; i < argument.length; i++) {
+        while (arr.includes(argument[i]) == true) {
+            arr.splice(arr.indexOf(argument[i]), 1);
+        }
+    };
+    return arr;
 };
 
 // Do not edit below this line

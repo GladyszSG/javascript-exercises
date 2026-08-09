3b3792424224c1b9f6b10c8f8c5a37951739964d
GladyszSG <189050190+GladyszSG@users.noreply.github.com>
2026-08-09 23:12:58 +0200

    Finish exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+function sumAll(start, end) {
+    if (Number.isInteger(start) == false || Number.isInteger(end) == false || start < 0 || end < 0) {
+        return "ERROR"
+    } else {
+        let sum = 0
+        if (start > end) {
+            [start, end] = [end, start]
+        };
+        for (let i = start; i <= end; i++) {
+            sum += i
+        };
+        return sum;
+    };
 };
 
 // Do not edit below this line
